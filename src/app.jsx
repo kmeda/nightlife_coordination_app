09d3d2ffe8c529d0e297ecfe18a2db27ecc916cb
Karthik Meda <karthikreddymeda@gmail.com>
2017-07-02 23:12:42 +0530

    initialize

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Home from './components/Home.jsx';
+
+import '../styles/main.scss';
+
+
+ReactDOM.render(
+  <Home />,
+  document.getElementById('root'));
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+class Home extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>Start Here.........</div>
+    )
+  }
+}
+
+export default Home;
