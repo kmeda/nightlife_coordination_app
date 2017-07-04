be640f41cd86095ecd4f0f0fa7f6ea19fe5960cb
Karthik Meda <karthikreddymeda@gmail.com>
2017-07-04 12:23:54 +0530

    UI tinkering

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -14,6 +14,11 @@ class Body extends Component {
           <div className="inner-container">
             <div className="header-container">
               <h1>Plans Tonite?</h1>
+              <div className="icons-styling">
+                <span className="fa fa-map-marker fa-2x"></span>
+                <span className="fa fa-cab fa-2x"></span>
+                <span className="fa fa-glass fa-2x"></span>
+              </div>
               <h2 className="content-h2">See which bars are hoppin tonight and RSVP ahead of time!</h2>
               <h2 className="content-h2">Remember: take a cab and drink responsibly.</h2>
 
@@ -23,9 +28,15 @@ class Body extends Component {
               </div>
             </div>
             <div className="list-container">
-              <span className="inline-text">Designed and developed by Karthik - Github Link</span>
+              <br/><br/><br/><br/>
+              <div className="list-content">Search for bars local to your area...</div>
+              <br/><br/><br/><br/>
+              <div className="list-content">Powered by Yelp</div>
             </div>
           </div>
+
+          <span className="inline-text">Designed and developed by Karthik   <a href="https://github.com/kmeda" target="_blank"><span className="fa fa-github fa-lg git-hub"></span></a></span>
+
         </div>
         </div>
     )
