f4523b07de861ee3d0f7be82efb24d7b6981766e
Karthik Meda <karthikreddymeda@gmail.com>
2017-07-07 19:19:39 +0530

    node server proxy to call yelp api

--- server.js
+++ server.js
@@ -34,18 +34,18 @@ app.get('/yelpapi', (req, res)=>{
   }).catch((error)=>console.log(error));
 });
 
-// if (process.env.NODE_ENV !== 'production') {
-//   const webpackMiddleware = require('webpack-dev-middleware');
-//   const webpack = require('webpack');
-//   const webpackConfig = require('./webpack.config.js');
-//   console.log("Runnig non-prod");
-//   app.use(webpackMiddleware(webpack(webpackConfig)));
-// } else {
-//
-//   app.use(express.static('dist'));
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'dist/index.html'));
-//   });
-// }
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const webpackConfig = require('./webpack.config.js');
+  console.log("Runnig non-prod");
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
 
 app.listen(process.env.PORT || 3050, () => console.log('Listening'));
