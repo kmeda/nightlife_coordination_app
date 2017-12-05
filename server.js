06cc22bd9435ba562da6759650022fcd590a7673
kmeda <karthikreddymeda@gmail.com>
2017-12-05 10:09:42 +0530

    minify build

--- server.js
+++ server.js
@@ -54,19 +54,10 @@ app.get('/yelpapi/reviews', (req, res)=>{
 
 });
 
+app.use(express.static('dist'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
-if (process.env.NODE_ENV !== 'production') {
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack.config.js');
-  console.log("Runnig non-prod");
-  app.use(webpackMiddleware(webpack(webpackConfig)));
-} else {
-
-  app.use(express.static('dist'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
-}
 
 app.listen(process.env.PORT || 3050, () => console.log('Listening'));
