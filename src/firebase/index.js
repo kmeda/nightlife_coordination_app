bd35aae76eb943489fc260b405c4b73361cae132
kmeda <karthikreddymeda@gmail.com>
2017-12-05 10:14:13 +0530

    firebase env err

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -9,6 +9,7 @@ try {
         storageBucket: process.env.STORAGE_BUCKET,
         messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
+    console.log(config);
     firebase.initializeApp(config);
 } catch (e) {
 
